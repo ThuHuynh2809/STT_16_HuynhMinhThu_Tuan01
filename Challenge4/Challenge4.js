b6264ea4663d8d3c67aa3cadd516e8645de9640e
ThuHuynh2809 <124646995+ThuHuynh2809@users.noreply.github.com>
2023-09-16 18:26:27 +0700

    ThuHuynh2809
    up

--- Challenge4/Challenge4.js
+++ Challenge4/Challenge4.js
@@ -3,26 +3,10 @@ var giaTriHoaDon1 = 275;
 var giaTriHoaDon2 = 40;
 var giaTriHoaDon3 = 430;
 
-// Công thức tính tiền tip 15%, tip 20%.
-function tip15 (giaTriHoaDon){
-    return giaTriHoaDon * 15/100;
-}
-function tip20 (giaTriHoaDon){
-    return giaTriHoaDon * 20/100;
-}
-
-// Điều kiện tính tiền tip.
-function dieuKienTinhTienTip(giaTriHoaDon){
-    if(giaTriHoaDon >= 50 &&  giaTriHoaDon <= 300)
-        return tip15(giaTriHoaDon);
-    else
-        return tip20(giaTriHoaDon);
-}
-
 // Tính tiền tip cho các hóa đơn
-var tienBoa1 = dieuKienTinhTienTip(giaTriHoaDon1);
-var tienBoa2 = dieuKienTinhTienTip(giaTriHoaDon2);
-var tienBoa3 = dieuKienTinhTienTip(giaTriHoaDon3);
+var tienBoa1 = giaTriHoaDon1 >= 50 && giaTriHoaDon1 <= 300 ? giaTriHoaDon1 * 0.15 : giaTriHoaDon1 *0.2;
+var tienBoa2 = giaTriHoaDon2 >= 50 && giaTriHoaDon2 <= 300 ? giaTriHoaDon2 * 0.15 : giaTriHoaDon2 *0.2;
+var tienBoa3 = giaTriHoaDon3 >= 50 && giaTriHoaDon3 <= 300 ? giaTriHoaDon3 * 0.15 : giaTriHoaDon3 *0.2;
 
 
 // In kết quả ra mang hình
